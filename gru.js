b13b6067c387e33a1954b91462ee36f28b4e8910
okejek323 <144665821+okejek323@users.noreply.github.com>
2023-11-08 13:56:51 +0100

    Add files via upload

---
+++ gru.js
@@ -0,0 +1,14 @@
+
+function swiatlo(x) {
+  var pic;
+  if (x == 0) {
+    pic = "wylonczona.gif"
+  } 
+  else {
+    pic = "wlenczona.gif"
+  }
+  document.getElementById('myImage').src = pic;
+}
+
+
+
